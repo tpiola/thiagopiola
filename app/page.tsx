3cd4d325012aeeefe4bae9fff44eb6e84edadd0b
Thiago Piola <contato@thiagopiola.com.br>
2026-06-14 20:18:02 +0000

    feat: scroll-driven 3D showcase com sticky scrub e cards orbitais

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { PageShell } from "@/components/motion/PageShell";
 import { Projetos } from "@/components/Projetos";
+import { ScrollShowcase3D } from "@/components/ScrollShowcase3D";
 import { Tecnologia } from "@/components/Tecnologia";
 import { Trajetoria } from "@/components/Trajetoria";
 import { TrustBar } from "@/components/TrustBar";
@@ -20,6 +21,7 @@ export default function Home() {
         <Autoridade />
         <Competencias />
         <Projetos />
+        <ScrollShowcase3D />
         <Tecnologia />
         <Trajetoria />
         <Footer />
