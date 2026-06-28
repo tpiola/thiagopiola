2e11a518bb2c1401783d012dea95e4339a5410bf
Thiago Piola <thiago@thiagopiola.com.br>
2026-06-28 00:25:30 +0000

    chore: config inicial

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   transpilePackages: ["lucide-react"],
   images: {
     formats: ["image/avif", "image/webp"],
